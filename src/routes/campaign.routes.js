837d00950e842312cf535452dd6e515f2bbb3ece
andresperdomo408 <124933026+andresperdomo408@users.noreply.github.com>
2023-12-05 22:35:26 -0500

    commit template create

--- src/routes/campaign.routes.js
+++ src/routes/campaign.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { getCampaign, getCampaignGroup, createTemplateText } from '../controllers/campaign.controller.js'
+import { getCampaign, getCampaignGroup, postTemplate } from '../controllers/campaign.controller.js'
 const router = Router()
 
 router.post('/campaign-dashboard', getCampaign);
 router.get('/campaignGroup', getCampaignGroup);
-router.post('/template', createTemplateText);
+router.post('/template', postTemplate);
 
 export default router

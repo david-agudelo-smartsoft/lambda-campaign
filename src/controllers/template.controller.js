4e6d8eb7a9bc4edf01e800906503150f3e0d2adf
DavidOrozco74 <144831006+DavidOrozco74@users.noreply.github.com>
2023-12-19 08:10:35 -0500

    02-DO-AJUSTE CONTROLADOR TEMPLATE GUPSHUP

--- src/controllers/template.controller.js
+++ src/controllers/template.controller.js
@@ -1,9 +1,25 @@
 import axios from "axios";
 import qs from "querystring";
+import fs from "fs";
 
 export const postTemplateText = async (req) => {
-    try {
-      const {
+  try {
+    const {
+      languageCode,
+      content,
+      category,
+      templateType,
+      elementName,
+      exampleHeader,
+      example,
+      vertical,
+      allowTemplateCategoryChange,
+    } = req.body.campaignTemplate.variables;
+    console.log(req.body.campaignTemplate.variables);
+
+    const apiResponse = await axios.post(
+      "https://api.gupshup.io/wa/app/e16ef554-0930-4b6e-88bd-6e948ce5f78a/template",
+      {
         languageCode,
         content,
         category,
@@ -13,88 +29,104 @@ export const postTemplateText = async (req) => {
         example,
         vertical,
         allowTemplateCategoryChange,
-      } = req.body.campaignTemplate.variables;
-      console.log(req.body.campaignTemplate.variables);
-  
-      const apiResponse = await axios.post(
-        "https://api.gupshup.io/wa/app/e16ef554-0930-4b6e-88bd-6e948ce5f78a/template",
-        {
-          languageCode,
-          content,
-          category,
-          templateType,
-          elementName,
-          exampleHeader,
-          example,
-          vertical,
-          allowTemplateCategoryChange,
+      },
 
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          apikey: "bbcms7lbqmxe70qy89kj1jxcfmnjfaze",
         },
-  
+      }
+    );
+
+    console.log(apiResponse.data);
+
+    if (apiResponse.data.status === "success") {
+      const firstApiTemplateId = apiResponse.data.template.id;
+      const contentFromReqBody = apiResponse.data.template.data;
+
+      const { project } = req.body.campaignTemplate.project;
+      const { title } = req.body.campaignTemplate.variables;
+      const { idGupshup } = req.body.campaignTemplate.variables;
+      const token = req.body.campaignTemplate.token;
+
+      // Puedes enviar la respuesta a otra API aquí
+      const secondApiResponse = await axios.post(
+        "https://agentechatdevmgt.smartsoft.com.co:3000/campaign-templates",
+        {
+          campaignTemplate: {
+            channel: "GUPSHUP",
+            type: "TEXT",
+            message: ` ${contentFromReqBody}`,
+            project: `${project}`, // Usa el id del primer API response como project
+            externalIntegrationInfo: {
+              id: firstApiTemplateId,
+              params: ["Pruebas", "pruebas@correo.com"],
+            },
+            title: `${title}`,
+            idGupshup: `${idGupshup}`,
+          },
+        },
         {
           headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            apikey: "bbcms7lbqmxe70qy89kj1jxcfmnjfaze",
+            "Content-Type": "application/json",
+            Authorization: `${token}`,
+            // Otras cabeceras necesarias para la segunda API
           },
         }
       );
-  
-      console.log(apiResponse.data);
-  
-      if (apiResponse.data.status === "success") {
-        const firstApiTemplateId = apiResponse.data.template.id;
-        const contentFromReqBody = apiResponse.data.template.data;
-  
-        // Puedes enviar la respuesta a otra API aquí
-        const secondApiResponse = await axios.post(
-          "https://agentechatdevmgt.smartsoft.com.co:3000/campaign-templates",
-          {
-            campaignTemplate: {
-              channel: "GUPSHUP",
-              type: "TEXT",
-              message: ` ${contentFromReqBody}`,
-              project: "60ad701657b4fe0013d4d6ec", // Usa el id del primer API response como project
-              externalIntegrationInfo: {
-                id: firstApiTemplateId,
-                params: ["Pruebas", "pruebas@correo.com"],
-              },
-            },
-          },
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTI0NjZiNzkzOGEyZDAwMTM4NjQzM2EiLCJuYW1lIjp7ImZpcnN0TmFtZSI6ImFkbWluIiwibGFzdE5hbWUiOiIyIn0sInJvbGUiOiJBRE1JTiIsImNvbXBhbnkiOiI2MGFkNzAxNjU3YjRmZTAwMTNkNGQ2ZTkiLCJsYXN0TG9naW4iOiIyMDIzLTEyLTE0VDE1OjI1OjM5LjgzNloiLCJmaXJzdExvZ2luIjpmYWxzZSwiaWF0IjoxNzAyNTY3NTQwLCJleHAiOjE3MDI2NTM5NDB9.33hULgQlIQ6FUL3ZcoglqPM32zZ0yfnI9NjN9VZAEXA",
-              // Otras cabeceras necesarias para la segunda API
-            },
-          }
-        );
-        
-      } else {
-      }
-      console.log("La segunda API respondió:", secondApiResponse.data);
 
-    } catch (error) {
-
-     
-      
+      console.log("La segunda API respondió:", secondApiResponse);
+    } else {
     }
-  };
+  } catch (error) {
+    console.error("Error al realizar la solicitud a la API externa:", error);
+    throw new Error("Error interno del servidor: " + error.message);
+  }
+};
 
-  export const postTemplateImage = async (req, requestData) => {
-    try {
-      const {
-        languageCode,
-        content,
-        category,
-        templateType,
-        elementName,
-        example,
-        vertical,
-      } = req.body;
-      console.log(req.body);
-  
-      const requestData = {
+export const image = async (req) => {
+  try {
+    const image = req.file;
+    console.log("image", image);
+
+    const api = await axios.post(
+      "https://api.gupshup.io/wa/e16ef554-0930-4b6e-88bd-6e948ce5f78a/wa/media",
+      { image },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          apikey: "bbcms7lbqmxe70qy89kj1jxcfmnjfaze",
+        },
+      }
+    );
+
+    console.log(api.data);
+  } catch (error) {
+    console.error("Error al realizar la solicitud a la API externa:", error);
+    throw new Error("Error interno del servidor: " + error.message);
+  }
+};
+
+export const postTemplateImage = async (req, requestData) => {
+  try {
+    const {
+      languageCode,
+      content,
+      category,
+      templateType,
+      elementName,
+      example,
+      vertical,
+      allowTemplateCategoryChange,
+      exampleMedia,
+      mediaId,
+    } = req.body;
+    console.log(req.body);
+
+    const apiResponse = await axios.post(
+      "https://api.gupshup.io/wa/app/e16ef554-0930-4b6e-88bd-6e948ce5f78a/template",
+      {
         languageCode,
         content,
         category,
@@ -103,59 +135,104 @@ export const postTemplateText = async (req) => {
         exampleHeader,
         example,
         vertical,
-      };
-  
-      const requestBody = qs.stringify(requestData);
-  
-      const apiResponse = await axios.post(
-        "https://api.gupshup.io/wa/app/e16ef554-0930-4b6e-88bd-6e948ce5f78a/template",
-        requestBody,
+        allowTemplateCategoryChange,
+        exampleMedia,
+        mediaId,
+      },
+
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          apikey: "bbcms7lbqmxe70qy89kj1jxcfmnjfaze",
+        },
+      }
+    );
+
+    console.log(apiResponse.data);
+
+    if (apiResponse.data.status === "success") {
+      const firstApiTemplateId = apiResponse.data.template.id;
+      const contentFromReqBody = apiResponse.data.template.data;
+
+      // Puedes enviar la respuesta a otra API aquí
+      const secondApiResponse = await axios.post(
+        "https://agentechatdevmgt.smartsoft.com.co:3000/campaign-templates",
+        {
+          campaignTemplate: {
+            channel: "GUPSHUP",
+            type: "TEXT",
+            message: ` ${contentFromReqBody}`,
+            project: "60ad701657b4fe0013d4d6ec", // Usa el id del primer API response como project
+            externalIntegrationInfo: {
+              id: firstApiTemplateId,
+              params: ["Pruebas", "pruebas@correo.com"],
+            },
+          },
+        },
         {
           headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            apikey: "bbcms7lbqmxe70qy89kj1jxcfmnjfaze",
+            "Content-Type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTI0NjZiNzkzOGEyZDAwMTM4NjQzM2EiLCJuYW1lIjp7ImZpcnN0TmFtZSI6ImFkbWluIiwibGFzdE5hbWUiOiIyIn0sInJvbGUiOiJBRE1JTiIsImNvbXBhbnkiOiI2MGFkNzAxNjU3YjRmZTAwMTNkNGQ2ZTkiLCJsYXN0TG9naW4iOiIyMDIzLTEyLTE0VDE5OjIxOjI4LjM0MloiLCJmaXJzdExvZ2luIjpmYWxzZSwiaWF0IjoxNzAyNTgxNjg4LCJleHAiOjE3MDI2NjgwODh9.B_un2PGniztHFO9Ie3fYX3knQwh5KZDB3AknHvC8Alk",
+            // Otras cabeceras necesarias para la segunda API
           },
         }
       );
-  
-      console.log(apiResponse.data);
-  
-      if (apiResponse.data.status === "success") {
-        const firstApiTemplateId = apiResponse.data.template.id;
-        const contentFromReqBody = apiResponse.data.template.data;
-  
-        // Puedes enviar la respuesta a otra API aquí
-        const secondApiResponse = await axios.post(
-          "https://agentechatdevmgt.smartsoft.com.co:3000/campaign-templates",
-          {
-            campaignTemplate: {
-              channel: "GUPSHUP",
-              type: "TEXT",
-              message: ` ${contentFromReqBody}`,
-              project: "60ad701657b4fe0013d4d6ec", // Usa el id del primer API response como project
-              externalIntegrationInfo: {
-                id: firstApiTemplateId,
-                params: ["Pruebas", "pruebas@correo.com"],
-              },
-            },
-          },
+    } else {
+      console.error(
+        "La primera API respondió con un error:",
+        apiResponse.data.error
+      );
+    }
+  } catch (error) {
+    console.error("Error al realizar la solicitud a la API externa:", error);
+    throw new Error("Error interno del servidor: " + error.message);
+  }
+};
+
+export const getTemplate = async (req, res) => {
+  try {
+    const appname = "RayoColMexApp"; // Hardcoded appname as per your request
+    const apikey = "bbcms7lbqmxe70qy89kj1jxcfmnjfaze"; // Hardcoded apikey as per your request
+
+    const headers = {
+      apikey: apikey,
+    };
+
+    const data = await axios.get(
+      `https://api.gupshup.io/sm/api/v1/template/list/${appname}`,
+      { headers }
+    );
+
+    console.log(data.data);
+
+    if (data.data.status === "success") {
+      const jsonData = data.data;
+
+      if (jsonData.status === "success") {
+        const listTemplates = await axios.post(
+          "https://agentechatdevmgt.smartsoft.com.co:3000/campaign-templates", 
+          [],
           {
             headers: {
-              "Content-Type": "application/json",
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTI0NjZiNzkzOGEyZDAwMTM4NjQzM2EiLCJuYW1lIjp7ImZpcnN0TmFtZSI6ImFkbWluIiwibGFzdE5hbWUiOiIyIn0sInJvbGUiOiJBRE1JTiIsImNvbXBhbnkiOiI2MGFkNzAxNjU3YjRmZTAwMTNkNGQ2ZTkiLCJsYXN0TG9naW4iOiIyMDIzLTEyLTA1VDE2OjM3OjU3Ljc1NFoiLCJmaXJzdExvZ2luIjpmYWxzZSwiaWF0IjoxNzAxNzk0Mjc3LCJleHAiOjE3MDE4ODA2Nzd9.RX6ThUpvmjrci19ao7HYFYcL8LvhAe0MEzJwfCF4JpI",
-              // Otras cabeceras necesarias para la segunda API
-            },
+       
+              "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTI0NjZiNzkzOGEyZDAwMTM4NjQzM2EiLCJuYW1lIjp7ImZpcnN0TmFtZSI6ImFkbWluIiwibGFzdE5hbWUiOiIyIn0sInJvbGUiOiJBRE1JTiIsImNvbXBhbnkiOiI2MGFkNzAxNjU3YjRmZTAwMTNkNGQ2ZTkiLCJsYXN0TG9naW4iOiIyMDIzLTEyLTE4VDIyOjA4OjMxLjU2MloiLCJmaXJzdExvZ2luIjpmYWxzZSwiaWF0IjoxNzAyOTM3MzExLCJleHAiOjE3MDMwMjM3MTF9.6a5ruBldDYUfHOf21d9y-Juj9daWie5VsDQyH27wbQc", // Agrega el token si es necesario
+            }
           }
         );
-      } else {
-        console.error(
-          "La primera API respondió con un error:",
-          apiResponse.data.error
-        );
+        console.log(listTemplates.data);
+
+        const templateIdOK = listTemplates.data.filter((template) => template.id === idGupshupTemplate);
+
+        if (templateIdOK.length === 1) {
+          res.json(templateIdOK[0]); // Assuming you want to send the template as a response
+          return;
+        }
       }
-    } catch (error) {
-      console.error("Error al realizar la solicitud a la API externa:", error);
-      throw new Error("Error interno del servidor: " + error.message);
     }
-  };  
+    res.json(data.data); // Assuming you want to send the data as a response
+  } catch (error) {
+    console.error("Error al realizar la solicitud a la API externa:", error);
+    throw new Error("Error interno del servidor: " + error.message);
+  }
+};
--- src/routes/template.routes.js
+++ src/routes/template.routes.js
@@ -1,8 +1,26 @@
 import { Router } from 'express';
-import { postTemplateImage,postTemplateText } from '../controllers/template.controller.js';
+import { postTemplateImage,postTemplateText,image,getTemplate } from '../controllers/template.controller.js';
+import multer from 'multer';
+import path from 'path';
+
+
 const router = Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, _file, cb) {
+     return cb(null, './uploads'); // Carpeta donde se guardarán las imágenes
+    },
+    filename: function (req, file, cb) {
+      return cb(null,`clone_${file.originalname}`) // Nombre del archivo con marca de tiempo para evitar conflictos de nombres
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  
 
 router.post('/template-image', postTemplateImage);
 router.post('/template', postTemplateText);
+router.post('/image',upload.single('image'),image)
+router.get('/templates', getTemplate);
 
 export default router;

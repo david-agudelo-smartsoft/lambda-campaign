9f19f3c1903c2639421f170e54718f8ab66e5262
andresperdomo408 <124933026+andresperdomo408@users.noreply.github.com>
2023-12-29 14:20:33 -0600

    fix querys

--- src/controllers/campaign.controller.js
+++ src/controllers/campaign.controller.js
@@ -1,18 +1,23 @@
 import OutboundCampaign from "../models/campaign.model.js";
+import { ObjectId } from 'mongodb';
 
 
 export const Detailsmessage = async (req, res) => {
   try {
-    const { initDate, finalDate,IdCampana  } = req.body;
+    const { initDate, finalDate ,projectId,campaignTemplateId } = req.body;
+
+  console.log(req.body)
 
     const result = await OutboundCampaign.aggregate([
       {
         $match: {
           $and: [
-            { createdAt: { $gte: new Date(initDate) } },
-            { createdAt: { $lte: new Date(finalDate) } },
+              { "campaignTemplate": new ObjectId(campaignTemplateId) },
+              { "project": new ObjectId(projectId), },
+              { "updatedAt": { $gte: new Date(initDate) } },
+              { "updatedAt": { $lte: new Date(finalDate) } }
           ],
-        },
+      },
       },
       { $sort: { createdAt: -1 } },
       {
@@ -158,12 +163,8 @@ export const Detailsmessage = async (req, res) => {
         },
       },
     ]);
-    let filteredResult = result;
-
-    if (IdCampana) {
-      filteredResult = filteredResult.filter(item => item.IdCampana == IdCampana);
-    }
-    res.json(filteredResult);
+   
+    res.json(result);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -171,16 +172,18 @@ export const Detailsmessage = async (req, res) => {
 
 export const DetailsmessageStatus = async (req, res) => {
   try {
-    const { initDate, finalDate, IdCampana, channelCheck } = req.body;
+    const { initDate, finalDate ,projectId,campaignTemplateId,channelCheck } = req.body;
 
 console.log(req.body)
     const result = await OutboundCampaign.aggregate([
       {
         $match: {
           $and: [
-            { createdAt: { $gte: new Date(initDate) } },
-            { createdAt: { $lte: new Date(finalDate) } },
-          ],
+            { "campaignTemplate": new ObjectId(campaignTemplateId) },
+            { "project": new ObjectId(projectId), },
+            { "updatedAt": { $gte: new Date(initDate) } },
+            { "updatedAt": { $lte: new Date(finalDate) } }
+        ],
         },
       },
       { $sort: { createdAt: -1 } },
@@ -330,10 +333,7 @@ console.log(req.body)
   
     let filteredResult = result;
 
-    if (IdCampana) {
-      filteredResult = filteredResult.filter(item => item.IdCampana == IdCampana);
-    }
-    
+  
     if (channelCheck) {
       const allowedChannelChecks = Array.isArray(channelCheck) ? channelCheck : [channelCheck.toLowerCase()];
       filteredResult = filteredResult.filter(item => allowedChannelChecks.includes(item.channelCheck.toLowerCase()));
@@ -355,7 +355,7 @@ export const DashboardTemplate = async (req, res) => {
       {
         $match: {
           $and: [
-            {projectId  :projectId},
+            // { "project": new ObjectId(projectId), },
             { createdAt: { $gte: new Date(initDate) } },
             { createdAt: { $lte: new Date(finalDate) } },
           ],
@@ -435,7 +435,7 @@ export const DashboardTemplate = async (req, res) => {
       },
       {
         $group: {
-          _id: { title: '$templateDoc.title', Fecha: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt', timezone: 'America/Bogota' } } },
+          _id: { title: '$templateDoc.title',campaignTemplate :"$templateDoc._id",project:"$templateDoc.project", Fecha: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt', timezone: 'America/Bogota' } } },
           total: { $sum: 1 },
           pending: { $sum: { $cond: { if: { $eq: ['$messageDoc.status', 'PENDING'] }, then: 1, else: 0 } } },
           failed: { $sum: { $cond: { if: { $eq: ['$messageDoc.status', 'FAILED_SEND'] }, then: 1, else: 0 } } },
@@ -453,6 +453,8 @@ export const DashboardTemplate = async (req, res) => {
           _id: 0,
           Title: '$_id.title',
           Fecha: '$_id.Fecha',
+          campaignTemplateId:'$_id.campaignTemplate',
+          projectId:'$_id.project',
           TOTAL: '$total',
           PENDING: '$pending',
           FAILED_SEND: '$failed',
@@ -462,293 +464,6 @@ export const DashboardTemplate = async (req, res) => {
           CHANELL_SENT: '$channelSent',
           CHANELL_DELIVERED: '$channelDelivered',
           CHANELL_READ: '$channelRead',
-        },
-      },
-    ]);
-
-    res.json(result);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
-
-export const getCampaignGroup = async (req, res) => {
-  try {
-    const { initDate, finalDate } = req.body;
-
-    const result = await OutboundCampaign.aggregate([
-      {
-        $match: {
-          $and: [
-            { updatedAt: { $gte: new Date(initDate) } },
-            { updatedAt: { $lte: new Date(finalDate) } },
-            // { "project": new ObjectId(projectId) },
-          ],
-        },
-      },
-      {
-        $lookup: {
-          from: "campaign.templates",
-          localField: "campaignTemplate",
-          foreignField: "_id",
-          as: "templateDoc",
-        },
-      },
-      { $unwind: { path: "$templateDoc" } },
-      {
-        $lookup: {
-          from: "outbound.messages",
-          localField: "_id",
-          foreignField: "outboundCampaign",
-          as: "messageDoc",
-        },
-      },
-      { $unwind: { path: "$messageDoc" } },
-      {
-        $addFields: { eventDoc: { $arrayElemAt: ["$messageDoc.comments", 0] } },
-      },
-      {
-        $group: {
-          _id: {
-            title: "$templateDoc.title",
-            Fecha: {
-              $dateToString: {
-                format: "%Y-%m-%d",
-                date: "$createdAt",
-                timezone: "America/Bogota",
-              },
-            },
-          },
-          total: { $sum: 1 },
-          pending: {
-            $sum: {
-              $cond: {
-                if: { $eq: ["$messageDoc.status", "PENDING"] },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          failed: {
-            $sum: {
-              $cond: {
-                if: { $eq: ["$messageDoc.status", "FAILED_SEND"] },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          send: {
-            $sum: {
-              $cond: {
-                if: { $eq: ["$messageDoc.status", "SUCCESFULLY_SEND"] },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          channelPending: {
-            $sum: {
-              $cond: {
-                if: {
-                  $eq: [{ $ifNull: ["$eventDoc.event", "pending"] }, "pending"],
-                },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          channelSent: {
-            $sum: {
-              $cond: {
-                if: {
-                  $eq: [{ $ifNull: ["$eventDoc.event", "pending"] }, "sent"],
-                },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          channelFailed: {
-            $sum: {
-              $cond: {
-                if: {
-                  $eq: [{ $ifNull: ["$eventDoc.event", "pending"] }, "failed"],
-                },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          channelDelivered: {
-            $sum: {
-              $cond: {
-                if: {
-                  $eq: [
-                    { $ifNull: ["$eventDoc.event", "pending"] },
-                    "delivered",
-                  ],
-                },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          channelRead: {
-            $sum: {
-              $cond: {
-                if: {
-                  $eq: [{ $ifNull: ["$eventDoc.event", "pending"] }, "read"],
-                },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-        },
-      },
-      { $sort: { "_id.Fecha": 1 } },
-      {
-        $project: {
-          _id: 0,
-          Title: "$_id.title",
-          Fecha: "$_id.Fecha",
-          TOTAL: "$total",
-          PENDING: "$pending",
-          FAILED_SEND: "$failed",
-          SUCCESFULLY_SEND: "$send",
-          CHANELL_PENDING: "$channelPending",
-          CHANELL_SENT: "$channelSent",
-          CHANELL_FAILED: "$channelFailed",
-          CHANELL_DELIVERED: "$channelDelivered",
-          CHANELL_READ: "$channelRead",
-        },
-      },
-    ]).limit(20);
-    const { idCampana, channelCheck } = req.body;
-    let filteredResult = result;
-
-    if (idCampana) {
-      filteredResult = filteredResult.filter(item => item.IdCampana === idCampana);
-    }
-
-    if (channelCheck) {
-      filteredResult = filteredResult.filter(item => item.channelCheck === channelCheck);
-    }
-
-    res.json(result);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
-
-export const getCampaign = async (req, res) => {
-  try {
-    const { initDate, finalDate } = req.body;
-
-    const result = await OutboundCampaign.aggregate([
-      {
-        $match: {
-          $and: [
-            { updatedAt: { $gte: new Date(initDate) } },
-            { updatedAt: { $lte: new Date(finalDate) } },
-            // {"project": new ObjectId(projectId)},
-          ],
-        },
-      },
-      {
-        $lookup: {
-          from: "campaign.templates",
-          localField: "campaignTemplate",
-          foreignField: "_id",
-          as: "templateDoc",
-        },
-      },
-      { $unwind: { path: "$templateDoc" } },
-      {
-        $lookup: {
-          from: "outbound.messages",
-          localField: "_id",
-          foreignField: "outboundCampaign",
-          as: "messageDoc",
-        },
-      },
-      { $unwind: { path: "$messageDoc" } },
-      {
-        $lookup: {
-          from: "interactions",
-          localField: "messageDoc.interactionId",
-          foreignField: "_id",
-          as: "interactionDoc",
-        },
-      },
-      {
-        $unwind: { path: "$interactionDoc", preserveNullAndEmptyArrays: true },
-      },
-      {
-        $group: {
-          _id: {
-            title: "$templateDoc.title",
-            Campana: "$_id",
-            Fecha: {
-              $dateToString: {
-                format: "%Y-%m-%d %H:%M:%S",
-                date: "$createdAt",
-                timezone: "America/Bogota",
-              },
-            },
-          },
-          total: { $sum: 1 },
-          send: {
-            $sum: {
-              $cond: {
-                if: { $eq: ["$messageDoc.status", "SUCCESFULLY_SEND"] },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          check: {
-            $sum: {
-              $cond: {
-                if: { $eq: ["$interactionDoc.delivered", true] },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          pending: {
-            $sum: {
-              $cond: {
-                if: { $eq: ["$messageDoc.status", "PENDING"] },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-          failed: {
-            $sum: {
-              $cond: {
-                if: { $eq: ["$messageDoc.status", "FAILED_SEND"] },
-                then: 1,
-                else: 0,
-              },
-            },
-          },
-        },
-      },
-      { $sort: { "_id.Fecha": -1 } },
-      {
-        $project: {
-          _id: 0,
-          Title: "$_id.title",
-          IdCampana: "$_id.Campana",
-          Fecha: "$_id.Fecha",
-          TOTAL: "$total",
-          PENDING: "$pending",
-          SUCCESFULLY_SEND: "$send",
-          FAILED_SEND: "$failed",
-          CHECK_CHANNEL: "$check",
         },
       },
     ]);
--- src/routes/campaign.routes.js
+++ src/routes/campaign.routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { getCampaign, getCampaignGroup,Detailsmessage,DashboardTemplate,DetailsmessageStatus } from '../controllers/campaign.controller.js'
+import {Detailsmessage,DashboardTemplate,DetailsmessageStatus } from '../controllers/campaign.controller.js'
 const router = Router()
 
-router.post('/campaign-dashboard', getCampaign);
+
 router.post('/Detailsmessage', Detailsmessage);
 router.post('/DetailsmessageStatus', DetailsmessageStatus);
 router.post('/Dashboardtemplate', DashboardTemplate);
-router.get('/campaignGroup', getCampaignGroup);
+
 
 
 export default router
